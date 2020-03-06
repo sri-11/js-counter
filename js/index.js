cd292293afca0707c69c4491029df1bde277bb05
sri-11 <61681296+sri-11@users.noreply.github.com>
2020-03-06 16:15:43 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let val=0;
+let op=document.getElementById('t1');
+op.value=0;
+let add=document.getElementById('b1');
+let sub=document.getElementById('b2');
+
+function inc(){
+
+    if(op.value<10){
+        val=op.value++;
+        op.innerText=val;
+    }
+    else{
+    add.disabled = true;
+    }
+}
+
+function dec(){
+    if(op.value<=10 && op.value>0){
+        val=op.value--;
+        op.innerText=val;
+}
+else{
+    sub.disabled = true;
+    }
+}
+add.addEventListener("click",inc);
+sub.addEventListener("click",dec);
